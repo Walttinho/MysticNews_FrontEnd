a317b4d0d6e53ada5c62112e3e65f50c7b56fb51
Walttinho <walter.netto@live.com>
2023-06-18 19:12:21 -0300

    feat: data mock

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+export const news = [
+  {
+    title: "Lorem Ipsum 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie massa et leo imperdiet, at varius arcu maximus. Morbi aliquet diam mauris, nec tempor ex aliquet vel. Nam in purus odio. Duis sit amet lacus vitae diam fringilla laoreet ut id mauris. Morbi ac ligula vitae quam euismod volutpat. Nullam et cursus lectus, non rhoncus lectus. Nam leo nibh, vehicula eget lacinia non, tristique sed dui. Integer malesuada elementum egestas.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comnents: 5,
+  }, {
+    title: "Lorem Ipsum 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie massa et leo imperdiet, at varius arcu maximus. Morbi aliquet diam mauris, nec tempor ex aliquet vel. Nam in purus odio. Duis sit amet lacus vitae diam fringilla laoreet ut id mauris. Morbi ac ligula vitae quam euismod volutpat. Nullam et cursus lectus, non rhoncus lectus. Nam leo nibh, vehicula eget lacinia non, tristique sed dui. Integer malesuada elementum egestas.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comnents: 5,
+  }, {
+    title: "Lorem Ipsum 3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie massa et leo imperdiet, at varius arcu maximus. Morbi aliquet diam mauris, nec tempor ex aliquet vel. Nam in purus odio. Duis sit amet lacus vitae diam fringilla laoreet ut id mauris. Morbi ac ligula vitae quam euismod volutpat. Nullam et cursus lectus, non rhoncus lectus. Nam leo nibh, vehicula eget lacinia non, tristique sed dui. Integer malesuada elementum egestas.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comnents: 5,
+  }, {
+    title: "Lorem Ipsum 4",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie massa et leo imperdiet, at varius arcu maximus. Morbi aliquet diam mauris, nec tempor ex aliquet vel. Nam in purus odio. Duis sit amet lacus vitae diam fringilla laoreet ut id mauris. Morbi ac ligula vitae quam euismod volutpat. Nullam et cursus lectus, non rhoncus lectus. Nam leo nibh, vehicula eget lacinia non, tristique sed dui. Integer malesuada elementum egestas.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comnents: 5,
+  },
+];
