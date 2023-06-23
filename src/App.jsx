501d2490c3ac7a0baa47ef61093f6cd0bf549551
Walttinho <walter.netto@live.com>
2023-06-22 23:19:56 -0300

    add GlobalStyled

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { GlobalStyled } from './GlobalStyled'
 import Home from './Pages/Home/Home'
 
 function App() {
@@ -6,7 +7,10 @@ function App() {
   
   return (
     //html with javascript
+   <>
+   <GlobalStyled/>
    <Home/>
+   </>
   )
 }
 
